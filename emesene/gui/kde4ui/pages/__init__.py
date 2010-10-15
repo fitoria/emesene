15a00436586559d4a9a2207aff7de4de815782e8
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2010-10-15 22:25:05 +0200

    Revert "kde4: First stub of a Kde4  gui for emesene2. Only a (partially) working login screen for now. It is however possible to login with a given status."
    This reverts commit 2e843a10eab59254472f4bbc2eff2f29db014a98.

--- emesene/gui/kde4ui/pages/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-'''
-Module containing pages pluggable into a top level window
-'''
-
-# pylint: disable=W0403
-from LoginPage import LoginPage
--- emesene/gui/kde4ui/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-'''
-Module containing various widgets used throughout the frontend
-'''
-
-# pylint: disable=W0403
-from DisplayPicWidget     import DisplayPic
-from PresenceComboWidget  import PresenceCombo
